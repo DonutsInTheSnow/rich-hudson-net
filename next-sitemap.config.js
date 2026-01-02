6289df5cc5d27dfd27863411fd7e25b1f559cc97
Rich <rhudson4966@gmail.com>
2026-01-02 09:57:12 -0500

    add robots.txt to public and installed next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://richhudson.net',
+  generateRobotsTxt: true, // Also generates robots.txt!
+  sitemapSize: 7000,
+};
